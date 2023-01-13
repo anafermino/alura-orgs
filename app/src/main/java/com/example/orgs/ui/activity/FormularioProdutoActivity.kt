0225dff1413140bf8c4e5a207c26d96e9c281b0e
Ana Lucia Fermino de O. Arine <CIANDT\anafermino@lnb018676cps.ciandt.global>
2023-01-13 10:07:37 -0300

    Criados binding dos edit text

--- app/src/main/java/com/example/orgs/ui/activity/FormularioProdutoActivity.kt
+++ app/src/main/java/com/example/orgs/ui/activity/FormularioProdutoActivity.kt
@@ -2,7 +2,10 @@ package com.example.orgs.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.example.orgs.databinding.ActivityFormularioProdutoBinding
+import com.example.orgs.model.Produto
+import java.math.BigDecimal
 
 class FormularioProdutoActivity : AppCompatActivity() {
 
@@ -13,5 +16,26 @@ class FormularioProdutoActivity : AppCompatActivity() {
         binding = ActivityFormularioProdutoBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+
+//        val nome = binding.nome.text.toString()
+//        Log.i("FormularioProdutoActivity", "onCreate: $nome")
+
+        binding.btnSaveData.setOnClickListener {
+            val nome = binding.nome.text.toString()
+            val descricao = binding.descricao.text.toString()
+            val valorEmTexto = binding.valor.text.toString()
+            val valor = if(valorEmTexto.isBlank()) {
+                BigDecimal.ZERO
+            } else {
+                BigDecimal(valorEmTexto)
+            }
+
+            val produtoNovo = Produto(
+                name = nome,
+                description = descricao,
+                price = valor
+            )
+            Log.i("FormularioProdutoActivity", "onCreate: $produtoNovo")
+        }
     }
 }
